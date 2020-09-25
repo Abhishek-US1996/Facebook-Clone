ffdb52ad5079d0b05119f8acbab5e86846bf2bc3
Uriti Sai Abhishek <contact@uritisaiabhishek.online>
2020-09-25 09:18:58 +0530

    navbar initial setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./Components/Header/Header";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<Router>
+				<Header />
+				<Switch>
+					<Route exact path='/'>
+						Home
+					</Route>
+					<Route exact path='/Market'>
+						Market Place
+					</Route>
+					<Route exact path='/Groups'>
+						Groups Place
+					</Route>
+				</Switch>
+			</Router>
+		</>
+	);
 }
 
 export default App;
